e26adc9fee95d34f52aabaf4db32b16965d7b2ba
guajiropa <guajiropa@hotmail.com>
2018-02-21 22:36:03 -0500

    Updated Entity Framework methods to deal with changes made to the models

--- ComicBookGalleryModel/Context.cs
+++ ComicBookGalleryModel/Context.cs
@@ -10,6 +10,17 @@ namespace ComicBookGalleryModel
 {
     public class Context : DbContext
     {
+        public Context()
+        {
+            // Drop and recreate the database if the model has changed.
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+
+            // Create the database if it does not exists.
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+
+            // Drop and create the database everytime that the app is ran
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+        }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
 
